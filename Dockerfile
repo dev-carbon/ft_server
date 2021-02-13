0cddace57c7ea085074b3758c7e06568c5b43d19
dev-carbon <humanfou@student.42.fr>
2021-02-13 14:26:00 +0100

    groovy push

---
+++ Dockerfile
@@ -0,0 +1,31 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Dockerfile                                         :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: humanfou <humanfou@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2021/01/11 21:24:17 by humanfou          #+#    #+#              #
+#    Updated: 2021/01/21 11:24:13 by humanfou         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+FROM debian:buster
+
+WORKDIR /tmp
+
+RUN apt-get update && apt-get install -y \
+    mariadb-server \
+    php7.3 php-mysql php-fpm php-pdo php-gd php-cli php-mbstring \
+    nginx \
+    wget \
+    nano && \
+    wget https://wordpress.org/latest.tar.gz && \
+    wget https://files.phpmyadmin.net/phpMyAdmin/5.0.4/phpMyAdmin-5.0.4-all-languages.tar.gz
+
+COPY ./srcs/phpmyadmin.config.php .
+COPY ./srcs/wordpress.config.php .
+COPY ./srcs/ft_server .
+COPY ./srcs/init.sh .
+
+CMD ["bash", "init.sh"]
---
+++ srcs/init.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Check for autoindex config
+if [[ $NGINX_AUTO_INDEX = "on" ]]
+then
+    sed -i '21s/.*/autoindex on;/' /tmp/ft_server
+elif [[ $NGINX_AUTO_INDEX = "off" ]]
+then
+    sed -i '21s/.*/autoindex off;/' /tmp/ft_server
+fi
+
+# Start services
+service mysql start
+
+# Configure a wordpress database
+echo "CREATE DATABASE wordpress_db;"| mysql -u root --skip-password
+echo "GRANT ALL PRIVILEGES ON wordpress_db.* TO 'root'@'localhost' WITH GRANT OPTION;"| mysql -u root --skip-password
+echo "FLUSH PRIVILEGES;"| mysql -u root --skip-password
+echo "update mysql.user set plugin='' where user='root';"| mysql -u root --skip-password
+
+# SSL
+mkdir /etc/nginx/ssl
+openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out /etc/nginx/ssl/ft_server.pem -keyout /etc/nginx/ssl/ft_server.key -subj "/C=FR/ST=Paris/L=Paris/O=42 School/OU=humanfou/CN=ft_server"
+
+
+# Copy nginx default server configuration
+rm /etc/nginx/sites-available/default && rm /etc/nginx/sites-enabled/default
+mv ./ft_server /etc/nginx/sites-available/.
+ln -s /etc/nginx/sites-available/ft_server /etc/nginx/sites-enabled
+
+# Install wordpress
+tar xvzf latest.tar.gz
+rm -f ./latest.tar.gz
+mv ./wordpress /var/www/html/.
+mv ./wordpress.config.php /var/www/html/wordpress/wp-config.php
+
+# Install phpmyadmin
+tar xvzf phpMyAdmin-5.0.4-all-languages.tar.gz
+rm  -f ./phpMyAdmin-5.0.4-all-languages.tar.gz
+mv ./phpMyAdmin-5.0.4-all-languages /usr/share/phpmyadmin
+mv ./phpmyadmin.config.php /usr/share/phpmyadmin/config.inc.php
+ln -s /usr/share/phpmyadmin /var/www/html
+
+# Create default index page
+touch /var/www/html/info.php
+echo "<?php phpinfo(); ?>" > /var/www/html/info.php
+
+service php7.3-fpm start
+service nginx start
+
+bash
